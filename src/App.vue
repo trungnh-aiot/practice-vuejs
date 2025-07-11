b5ab9dfe0f0c4233b59626b669f37489ea4959b5
trungnh-aiot <trungnh@aiot-global.com>
2025-07-11 13:51:54 +0700

    feat: set up tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,6 @@
 <script setup lang="ts"></script>
 
-<template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">index</RouterLink>
-        <RouterLink to="/home">Home</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
-</template>
+<template></template>
 
 <style scoped>
 header {
